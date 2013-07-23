26d8a74778d25ad1092233dbebe6d4a08f6f2234
Graham Campbell <graham@mineuk.com>
2013-07-24 00:21:35 +0100

    Changed the default queue

--- app/config/queue.php
+++ app/config/queue.php
@@ -37,7 +37,7 @@ return array(
         'beanstalkd' => array(
             'driver' => 'beanstalkd',
             'host'   => 'localhost',
-            'queue'  => 'default',
+            'queue'  => 'bootstrap-cms',
         ),
 
         'sqs' => array(
