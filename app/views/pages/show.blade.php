6f9d1060a361d253d513b2969a76f93e50fb0028
Graham Campbell <graham@mineuk.com>
2013-07-27 07:13:02 +0100

    Added page details for editors

--- app/views/pages/show.blade.php
+++ app/views/pages/show.blade.php
@@ -7,6 +7,28 @@
 
 @section('content')
 
+@if (Sentry::check() && Sentry::getUser()->hasAccess('edit'))
+    <div class="well clearfix">
+        <div class="span6">
+            <p>
+                <strong>Page Creator:</strong> {{ $page->user()->first()->email }}
+            </p>
+            <a class="btn btn-info" href="{{ URL::route('pages.edit', array('pages' => $page->slug)) }}">Edit Page</a>
+        </div>
+        <div class="span5">
+            <div class="pull-right">
+                <p>
+                    <em>Page Created: {{ $page->created_at }}</em>
+                </p>
+                <p>
+                    <em>Last Updated: {{ $page->updated_at }}</em>
+                </p>
+            </div>
+        </div>
+    </div>
+    <br>
+@endif
+
 <?php eval('?>'.$page->body.'<?'); ?>
 
 @stop
