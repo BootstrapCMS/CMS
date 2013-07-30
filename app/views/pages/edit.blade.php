9fff1c28e65a69777bf9af38db8e24891947dc25
Graham Campbell <graham@mineuk.com>
2013-07-30 01:37:30 +0100

    Use full page syntax

--- app/views/pages/edit.blade.php
+++ app/views/pages/edit.blade.php
@@ -17,7 +17,7 @@ Edit {{ $page->title }}
 
         <div class="span6">
             <div class="pull-right">
-                <a class="btn btn-success" href="{{ URL::route('pages.show', $page->slug) }}">Show Page</a> <a class="btn btn-danger action_confirm" href="{{ URL::route('pages.destroy', $page->slug) }}" data-token="{{ Session::getToken() }}" data-method="DELETE">Delete Page</a>
+                <a class="btn btn-success" href="{{ URL::route('pages.show', array('pages' => $page->slug)) }}">Show Page</a> <a class="btn btn-danger action_confirm" href="{{ URL::route('pages.destroy', array('pages' => $page->slug)) }}" data-token="{{ Session::getToken() }}" data-method="DELETE">Delete Page</a>
             </div>
         </div>
 <hr>
