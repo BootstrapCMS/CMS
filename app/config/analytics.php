76094f832030531fae9c2492314e8aee94990153
Graham Campbell <graham@mineuk.com>
2013-08-10 22:03:52 +0100

    Added google analytics support

---
+++ app/config/analytics.php
@@ -0,0 +1,33 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Google Analytics
+    |--------------------------------------------------------------------------
+    |
+    | This defines if Google Analytics is enabled.
+    |
+    | Requires a valid Google Analytics Tracking ID.
+    |
+    | Default to false. 
+    |
+    */
+
+    'google' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Google Analytics Tracking ID
+    |--------------------------------------------------------------------------
+    |
+    | This defines the Google Analytics Tracking ID to use.
+    |
+    | Default to 'UA-XXXXXXXX-X'.
+    |
+    */
+
+    'googleid' => 'UA-XXXXXXXX-X',
+
+);
