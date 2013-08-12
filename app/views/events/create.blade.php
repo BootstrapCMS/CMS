ff2e5edbf616aa70d1a08ce922751d4c7fa2e679
Graham Campbell <graham@mineuk.com>
2013-08-12 20:57:25 +0100

    Added some temp events code

---
+++ app/views/events/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('title')
+Create Event
+@stop
+
+@section('controls')
+<p class="lead">
+    This feature is not yet available.
+</p>
+@stop
---
+++ app/views/events/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('title')
+Edit {{ $event->getTitle() }}
+@stop
+
+@section('controls')
+<p class="lead">
+    This feature is not yet available.
+</p>
+@stop
