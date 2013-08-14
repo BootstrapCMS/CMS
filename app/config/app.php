ecbbfea03e17989afe54b4607232a46f4d75b0f3
Graham Campbell <graham@mineuk.com>
2013-08-14 12:48:26 +0100

    Refactored off the rest of the libs

--- app/config/app.php
+++ app/config/app.php
@@ -114,6 +114,8 @@ return array(
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
         'Cartalyst\Sentry\SentryServiceProvider',
+        'GrahamCampbell\Security\SecurityServiceProvider',
+        'GrahamCampbell\Binput\BinputServiceProvider'
         'GrahamCampbell\Passwd\PasswdServiceProvider',
         'GrahamCampbell\HTMLMin\HTMLMinServiceProvider',
         'VTalbot\Markdown\MarkdownServiceProvider',
@@ -188,6 +190,8 @@ return array(
         'Validator'       => 'Illuminate\Support\Facades\Validator',
         'View'            => 'Illuminate\Support\Facades\View',
         'Sentry'          => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
+        'Security'        => 'GrahamCampbell\Security\Facades\Security'
+        'Binput'          => 'GrahamCampbell\Binput\Facades\Binput'
         'Passwd'          => 'GrahamCampbell\Passwd\Facades\Passwd',
         'HTMLMin'         => 'GrahamCampbell\HTMLMin\Facades\HTMLMin',
         'Markdown'        => 'VTalbot\Markdown\Facades\Markdown',
