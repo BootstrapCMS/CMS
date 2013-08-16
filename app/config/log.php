3fa6aa72dde0004dde29df4c455bcf58e6ef2e1a
Graham Campbell <graham@mineuk.com>
2013-08-16 23:48:32 +0100

    Restored the correct config

--- app/config/log.php
+++ app/config/log.php
@@ -39,7 +39,7 @@ return array(
     |
     */
 
-    'illuminatequery' => true,
+    'illuminatequery' => false,
 
     /*
     |--------------------------------------------------------------------------
