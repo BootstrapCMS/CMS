847efa257ac60c994523828b2bf220454037c762
Graham Campbell <graham@mineuk.com>
2013-08-18 20:58:59 +0100

    Removed unused page provider function

--- app/src/GrahamCampbell/BootstrapCMS/Providers/PageProvider.php
+++ app/src/GrahamCampbell/BootstrapCMS/Providers/PageProvider.php
@@ -8,10 +8,4 @@ class PageProvider extends BaseProvider implements Interfaces\ISlugProvider {
 
     protected $model = 'GrahamCampbell\BootstrapCMS\Models\Page';
 
-    // public function all() {
-    //     if ('cache')
-    //     $model = $this->model;
-    //     return $model::get(array('id', 'user_id', 'created_at', 'updated_at', 'slug', 'title', 'icon'))->toArray();
-    // }
-
 }
