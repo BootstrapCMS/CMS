ed99a5b5214f84203fec3cb3122a366a6bd6da33
Graham Campbell <graham@mineuk.com>
2013-08-21 23:57:48 +0100

    Re-enabled event deletion

--- app/src/GrahamCampbell/BootstrapCMS/Models/Relations/Common/TraitHasManyEvents.php
+++ app/src/GrahamCampbell/BootstrapCMS/Models/Relations/Common/TraitHasManyEvents.php
@@ -21,8 +21,8 @@ trait TraitHasManyEvents {
     }
 
     public function deleteEvents() {
-        // foreach($this->getEvents(array('id')) as $event) {
-        //     $event->delete();
-        // }
+        foreach($this->getEvents(array('id')) as $event) {
+            $event->delete();
+        }
     }
 }
