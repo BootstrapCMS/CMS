15064686817afd1c744c5afe332849887ed8e4ba
Graham Campbell <graham@mineuk.com>
2013-08-25 21:18:19 +0100

    Updated the emails

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
 <p>Thank you for creating an account on <a href="{{ $url }}">{{ Config::get('cms.name') }}</a>.<p>
-<p>To activate your account, <a href="{{ $link }}">click here</a>,</p>
+<p>To activate your account, <a href="{{ $link }}">click here</a>.</p>
 @stop
