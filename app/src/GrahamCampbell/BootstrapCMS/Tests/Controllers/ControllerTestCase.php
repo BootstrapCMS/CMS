4d9b23b2be5d504947273d562eeafa8b59901e1c
Graham Campbell <graham@mineuk.com>
2013-10-21 22:43:46 +0100

    Test tweaks

--- app/src/GrahamCampbell/BootstrapCMS/Tests/Controllers/ControllerTestCase.php
+++ app/src/GrahamCampbell/BootstrapCMS/Tests/Controllers/ControllerTestCase.php
@@ -86,7 +86,7 @@ abstract class ControllerTestCase extends TestCase {
     }
 
     protected function setAsPage() {
-        Navigation::shouldReceive('addItem')->once();
+        Navigation::shouldReceive('addItem')->twice();
         Navigation::shouldReceive('get')->once()
             ->andReturn(array(
                 array('url' => 'http://localhost/pages/home', 'title' => 'Home', 'icon' => 'icon-home', 'active' => true),
