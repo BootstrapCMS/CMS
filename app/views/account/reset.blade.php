ba36019ffd36b656ea6d12c21473b091036d9f3a
Graham Campbell <graham@mineuk.com>
2013-10-24 22:27:03 +0100

    Updated the reset password page

--- app/views/account/reset.blade.php
+++ app/views/account/reset.blade.php
@@ -12,17 +12,20 @@ Reset Password
 <div class="well">
     {{ Form::open(array('url' => URL::route('account.reset.post'), 'method' => 'POST', 'class' => 'form-horizontal')) }}
 
-        <div class="control-group{{ ($errors->has('email')) ? ' error' : '' }}">
-            <label class="control-label" for="email">Email Address</label>
-            <div class="controls">
-                <input name="email" id="email" value="{{ Request::old('email') }}" type="text" class="input-xlarge" placeholder="Email Address">
+        <div class="form-group{{ ($errors->has('email')) ? ' has-error' : '' }}">
+            <label class="col-lg-2 control-label" for="email">Email Address</label>
+            <div class="col-lg-3">
+                <input name="email" id="email" value="{{ Request::old('email') }}" type="text" class="form-control" placeholder="Email Address">
                 {{ ($errors->has('email') ? $errors->first('email') : '') }}
             </div>
         </div>
 
-        <div class="form-actions">
-            <button class="btn btn-primary" type="submit"><i class="fa fa-rocket"></i> Reset Password</button>
+        <div class="form-group">
+            <div class="col-lg-offset-2 col-lg-6">
+                <button class="btn btn-primary" type="submit"><i class="fa fa-rocket"></i> Reset Password</button>
+            </div>
         </div>
+
     {{ Form::close() }}
 </div>
 @stop
