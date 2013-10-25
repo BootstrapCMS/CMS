51b0179dabcd301bc416b8ad885e85f475afc2c6
Graham Campbell <graham@mineuk.com>
2013-10-25 20:37:41 +0100

    Grid layout updates

--- app/views/account/register.blade.php
+++ app/views/account/register.blade.php
@@ -19,47 +19,47 @@ Register
         {{ Form::open(array('url' => URL::route('account.register.post'), 'method' => 'POST', 'class' => 'form-horizontal')) }}
 
             <div class="form-group{{ ($errors->has('first_name')) ? ' has-error' : '' }}">
-                <label class="col-lg-2 control-label" for="first_name">First Name</label>
-                <div class="col-lg-3">
+                <label class="col-sm-2 control-label" for="first_name">First Name</label>
+                <div class="col-sm-3">
                     <input name="first_name" id="first_name" value="{{ Request::old('first_name') }}" type="text" class="form-control" placeholder="First Name">
                     {{ ($errors->has('first_name') ? $errors->first('first_name') : '') }}
                 </div>
             </div>
 
             <div class="form-group{{ ($errors->has('last_name')) ? ' has-error' : '' }}">
-                <label class="col-lg-2 control-label" for="last_name">Last Name</label>
-                <div class="col-lg-3">
+                <label class="col-sm-2 control-label" for="last_name">Last Name</label>
+                <div class="col-sm-3">
                     <input name="last_name" id="last_name" value="{{ Request::old('last_name') }}" type="text" class="form-control" placeholder="Last Name">
                     {{ ($errors->has('last_name') ? $errors->first('last_name') : '') }}
                 </div>
             </div>
 
             <div class="form-group{{ ($errors->has('email')) ? ' has-error' : '' }}">
-                <label class="col-lg-2 control-label" for="email">Email</label>
-                <div class="col-lg-3">
+                <label class="col-sm-2 control-label" for="email">Email</label>
+                <div class="col-sm-3">
                     <input name="email" id="email" value="{{ Request::old('email') }}" type="text" class="form-control" placeholder="Email">
                     {{ ($errors->has('email') ? $errors->first('email') : '') }}
                 </div>
             </div>
 
             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label class="col-lg-2 control-label" for="password">Password</label>
-                <div class="col-lg-3">
+                <label class="col-sm-2 control-label" for="password">Password</label>
+                <div class="col-sm-3">
                     <input name="password" id="password" value="" type="password" class="form-control" placeholder="Password">
                     {{ ($errors->has('password') ?  $errors->first('password') : '') }}
                 </div>
             </div>
 
             <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                <label class="col-lg-2 control-label" for="password_confirmation">Confirm Password</label>
-                <div class="col-lg-3">
+                <label class="col-sm-2 control-label" for="password_confirmation">Confirm Password</label>
+                <div class="col-sm-3">
                     <input name="password_confirmation" id="password_confirmation" value="" type="password" class="form-control" placeholder="Confirm Password">
                     {{ ($errors->has('password_confirmation') ? $errors->first('password_confirmation') : '') }}
                 </div>
             </div>
 
             <div class="form-group">
-            <div class="col-lg-offset-2 col-lg-6">
+            <div class="col-sm-offset-2 col-sm-6">
                 <button class="btn btn-primary" type="submit"><i class="fa fa-rocket"></i> Register</button>
                 <button class="btn btn-inverse" type="reset">Reset</button>
             </div>
