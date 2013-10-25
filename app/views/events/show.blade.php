fdc9091ca37c6000adc639550ad2048bd7add6d2
Graham Campbell <graham@mineuk.com>
2013-10-26 00:04:09 +0100

    More styling and time

--- app/views/events/show.blade.php
+++ app/views/events/show.blade.php
@@ -7,20 +7,36 @@
 @section('controls')
 @if (Sentry::check() && Sentry::getUser()->hasAccess('edit'))
     <div class="well clearfix">
-        <div class="span6">
-            <p>
-                <strong>Event Creator:</strong> {{ $event->getUserEmail() }}
-            </p>
-            <a class="btn btn-info" href="{{ URL::route('events.edit', array('events' => $event->getId())) }}"><i class="fa fa-pencil-square-o"></i> Edit Event</a> <a class="btn btn-danger" href="#delete_event" data-toggle="modal" data-target="#delete_event"><i class="fa fa-times"></i> Delete Event</a>
+        <div class="hidden-xs">
+            <div class="col-sm-6">
+                <p>
+                    <strong>Event Creator:</strong> {{ $event->getUserEmail() }}
+                </p>
+                <a class="btn btn-info" href="{{ URL::route('events.edit', array('events' => $event->getId())) }}"><i class="fa fa-pencil-square-o"></i> Edit Event</a> <a class="btn btn-danger" href="#delete_event" data-toggle="modal" data-target="#delete_event"><i class="fa fa-times"></i> Delete Event</a>
+            </div>
+            <div class="col-sm-6">
+                <div class="pull-right">
+                    <p>
+                        <em>Event Created: <abbr class="timeago" title="{{ $event->getCreatedAt()->toISO8601String() }}">{{ $event->getCreatedAt()->toDateTimeString() }}</abbr></em>
+                    </p>
+                    <p>
+                        <em>Last Updated: <abbr class="timeago" title="{{ $event->getUpdatedAt()->toISO8601String() }}">{{ $event->getUpdatedAt()->toDateTimeString() }}</abbr></em>
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="span5">
-            <div class="pull-right">
+        <div class="visible-xs">
+            <div class="col-xs-12">
                 <p>
-                    <em>Event Created: {{ $event->getCreatedAt()->diffForHumans() }}</em>
+                    <strong>Event Creator:</strong> {{ $event->getUserEmail() }}
                 </p>
                 <p>
-                    <em>Last Updated: {{ $event->getUpdatedAt()->diffForHumans() }}</em>
+                    <strong>Event Created:</strong> <abbr class="timeago" title="{{ $event->getCreatedAt()->toISO8601String() }}">{{ $event->getCreatedAt()->toDateTimeString() }}</abbr>
                 </p>
+                <p>
+                    <strong>Last Updated:</strong> <abbr class="timeago" title="{{ $event->getUpdatedAt()->toISO8601String() }}">{{ $event->getUpdatedAt()->toDateTimeString() }}</abbr>
+                </p>
+                <a class="btn btn-info" href="{{ URL::route('events.edit', array('events' => $event->getId())) }}"><i class="fa fa-pencil-square-o"></i> Edit Event</a> <a class="btn btn-danger" href="#delete_event" data-toggle="modal" data-target="#delete_event"><i class="fa fa-times"></i> Delete Event</a>
             </div>
         </div>
     </div>
--- app/views/pages/show.blade.php
+++ app/views/pages/show.blade.php
@@ -12,20 +12,36 @@ if($page->getShowTitle() == false) {
 @section('controls')
 @if (Sentry::check() && Sentry::getUser()->hasAccess('edit'))
     <div class="well clearfix">
-        <div class="span6">
-            <p>
-                <strong>Page Creator:</strong> {{ $page->getUserEmail() }}
-            </p>
-            <a class="btn btn-info" href="{{ URL::route('pages.edit', array('pages' => $page->getSlug())) }}"><i class="fa fa-pencil-square-o"></i> Edit Page</a> <a class="btn btn-danger" href="#delete_page" data-toggle="modal" data-target="#delete_page"><i class="fa fa-times"></i> Delete Page</a>
+        <div class="hidden-xs">
+            <div class="col-sm-6">
+                <p>
+                    <strong>Page Creator:</strong> {{ $page->getUserEmail() }}
+                </p>
+                <a class="btn btn-info" href="{{ URL::route('pages.edit', array('pages' => $page->getSlug())) }}"><i class="fa fa-pencil-square-o"></i> Edit Page</a> <a class="btn btn-danger" href="#delete_page" data-toggle="modal" data-target="#delete_page"><i class="fa fa-times"></i> Delete Page</a>
+            </div>
+            <div class="col-sm-6">
+                <div class="pull-right">
+                    <p>
+                        <em>Page Created: <abbr class="timeago" title="{{ $page->getCreatedAt()->toISO8601String() }}">{{ $page->getCreatedAt()->toDateTimeString() }}</abbr></em>
+                    </p>
+                    <p>
+                        <em>Last Updated: <abbr class="timeago" title="{{ $page->getUpdatedAt()->toISO8601String() }}">{{ $page->getUpdatedAt()->toDateTimeString() }}</abbr></em>
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="span5">
-            <div class="pull-right">
+        <div class="visible-xs">
+            <div class="col-xs-12">
                 <p>
-                    <em>Page Created: {{ $page->getCreatedAt()->diffForHumans() }}</em>
+                    <strong>Page Creator:</strong> {{ $page->getUserEmail() }}
                 </p>
                 <p>
-                    <em>Last Updated: {{ $page->getUpdatedAt()->diffForHumans() }}</em>
+                    <strong>Page Created:</strong> <abbr class="timeago" title="{{ $page->getCreatedAt()->toISO8601String() }}">{{ $page->getCreatedAt()->toDateTimeString() }}</abbr>
                 </p>
+                <p>
+                    <strong>Last Updated:</strong> <abbr class="timeago" title="{{ $page->getUpdatedAt()->toISO8601String() }}">{{ $page->getUpdatedAt()->toDateTimeString() }}</abbr>
+                </p>
+                <a class="btn btn-info" href="{{ URL::route('pages.edit', array('pages' => $page->getSlug())) }}"><i class="fa fa-pencil-square-o"></i> Edit Page</a> <a class="btn btn-danger" href="#delete_page" data-toggle="modal" data-target="#delete_page"><i class="fa fa-times"></i> Delete Page</a>
             </div>
         </div>
     </div>
