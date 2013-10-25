0eabc1a941e6b9e99045460b558af832526c787a
Graham Campbell <graham@mineuk.com>
2013-10-25 22:17:42 +0100

    Further blogging updates

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -30,10 +30,10 @@
                 <strong>Post Creator:</strong> {{ $post->getUserEmail() }}
             </p>
             <p>
-                <em>Post Created: {{ $post->getCreatedAt()->diffForHumans() }}</em>
+                <strong>Post Created:</strong> {{ $post->getCreatedAt()->diffForHumans() }}
             </p>
             <p>
-                <em>Last Updated: {{ $post->getUpdatedAt()->diffForHumans() }}</em>
+                <strong>Last Updated:</strong> {{ $post->getUpdatedAt()->diffForHumans() }}
             </p>
             <a class="btn btn-info" href="{{ URL::route('blog.posts.edit', array('posts' => $post->getId())) }}"><i class="fa fa-pencil-square-o"></i> Edit Post</a> <a class="btn btn-danger" href="#delete_post" data-toggle="modal" data-target="#delete_post"><i class="fa fa-times"></i> Delete Post</a>
         </div>
@@ -41,16 +41,18 @@
     <hr>
 @endif
 
-<div class="col-sm-9 col-xs-8">
-    <p class="lead">
-        {{ $post->getSummary() }}
-    </p>
-</div>
-<div class="col-sm-3 col-xs-4">
-    <div class="pull-right">
-        <p>
-            Author: {{ $post->getUserName() }}
+<div class="row">
+    <div class="col-sm-9 col-xs-8">
+        <p class="lead">
+            {{ $post->getSummary() }}
         </p>
+    </div>
+    <div class="col-sm-3 col-xs-4">
+        <div class="pull-right">
+            <p>
+                Author: {{ $post->getUserName() }}
+            </p>
+        </div>
     </div>
 </div>
 <br>
@@ -66,16 +68,11 @@
 @if (Sentry::check() && Sentry::getUser()->hasAccess('user'))
     <br>
     {{ Form::open(array('url' => URL::route('blog.posts.comments.store', array('posts' => $post->getId())), 'method' => 'POST', 'class' => 'form-vertical')) }}
-
-        <div class="control-group">
-            <div class="controls">
-                <textarea id="body" name="body" class="span12, comment-box" placeholder="Type a comment..." rows="3"></textarea>
-            </div>
+        <div class="form-group">
+            <textarea id="body" name="body" class="span12, comment-box" placeholder="Type a comment..." rows="3"></textarea>
         </div>
-        <div class="control-group">
-            <div class="controls">
-                <button id="contact-submit" type="submit" class="btn btn-primary"><i class="fa fa-comment"></i> Post Comment</button>
-            </div>
+        <div class="form-group">
+            <button id="contact-submit" type="submit" class="btn btn-primary"><i class="fa fa-comment"></i> Post Comment</button>
         </div>
     {{ Form::close() }}
     <br>
@@ -90,8 +87,8 @@
 <p>There are currently no comments.</p>
 @else
     @foreach ($comments as $comment)
-        <div class="well">
-            <div class="col-md-10 col-sm-9 col-xs-8">
+        <div class="well row">
+            <div class="col-lg-10 col-md-9 col-sm-8 col-xs-12">
                 <p>
                     <strong>{{ $comment->getUserName() }}</strong> - {{ $comment->getCreatedAt()->diffForHumans() }}
                 </p>
@@ -99,13 +96,18 @@
                     {{ nl2br(e($comment->getBody())) }}
                 </p>
             </div>
-            <div class="col-md-2 col-sm-3 col-xs-4">
-                @if (Sentry::check() && Sentry::getUser()->hasAccess('mod'))
+            @if (Sentry::check() && Sentry::getUser()->hasAccess('mod'))
+                <div class="hidden-xs col-lg-2 col-md-3 col-sm-4">
                     <div class="pull-right">
                         <a class="btn btn-info" href="#edit_comment_{{ $comment->getId() }}" data-toggle="modal" data-target="#edit_comment_{{ $comment->getId() }}"><i class="fa fa-pencil-square-o"></i> Edit</a> <a class="btn btn-danger" href="#delete_comment_{{ $comment->getId() }}" data-toggle="modal" data-target="#delete_comment_{{ $comment->getId() }}"><i class="fa fa-times"></i> Delete</a>
                     </div>
-                @endif
-            </div>
+                </div>
+                <div class="visible-xs">
+                    <div class="pull-right">
+                        <a class="btn btn-info" href="#edit_comment_{{ $comment->getId() }}" data-toggle="modal" data-target="#edit_comment_{{ $comment->getId() }}"><i class="fa fa-pencil-square-o"></i> Edit</a> <a class="btn btn-danger" href="#delete_comment_{{ $comment->getId() }}" data-toggle="modal" data-target="#delete_comment_{{ $comment->getId() }}"><i class="fa fa-times"></i> Delete</a>
+                    </div>
+                </div>
+            @endif
         </div>
     @endforeach
 @endif
