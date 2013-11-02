4373503bd9815f1323e2cdd78ccabf1679eae3f3
Graham Campbell <graham@mineuk.com>
2013-11-02 01:28:06 +0000

    Delete locking

--- public/js/cms-comment-delete.js
+++ public/js/cms-comment-delete.js
@@ -1,38 +1,55 @@
+function cmsCommentDeleteSubmit(that) {
+    // TODO: message
+    $.ajax({
+        url: $(that).attr("href"),
+        type: "DELETE",
+        dataType: "json",
+        timeout: 5000,
+        success: function(data, status, xhr) {
+            if (!xhr.responseJSON) {
+                // TODO: message
+                cmsCommentLock = false;
+                return;
+            }
+            if (xhr.responseJSON.success !== true) {
+                // TODO: message
+                cmsCommentLock = false;
+                return;
+            }
+            if ($("#comments > div").length == 1) {
+                $("#comment_"+xhr.responseJSON.comment).fadeOut(300, function() {
+                    $(this).remove();
+                    $("<p id=\"nocomments\">There are currently no comments.</p>").prependTo("#comments").hide().fadeIn(300);
+                    cmsCommentLock = false;
+                }); 
+            } else {
+                $("#comment_"+xhr.responseJSON.comment).slideUp(300, function() {
+                    $(this).remove();
+                    cmsCommentLock = false;
+                });
+            }
+        },
+        error: function(xhr, status, error) {
+            if (!xhr.responseJSON) {
+                // TODO: message
+                cmsCommentLock = false;
+                return;
+            }
+        }
+    });
+}
+
 function cmsCommentDelete(bindval) {
     bindval = bindval || ".deletable";
     $(bindval).click(function() {
-        $.ajax({
-            url: $(this).attr("href"),
-            type: "DELETE",
-            dataType: "json",
-            timeout: 5000,
-            success: function(data, status, xhr) {
-                if (!xhr.responseJSON) {
-                    // TODO
-                    return;
-                }
-                if (xhr.responseJSON.success !== true) {
-                    // TODO
-                    return;
-                }
-                if ($("#comments > div").length == 1) {
-                    $("#comment_"+xhr.responseJSON.comment).fadeOut(300, function() {
-                        $(this).remove();
-                        $("<p id=\"nocomments\">There are currently no comments.</p>").prependTo("#comments").hide().fadeIn(300);
-                    }); 
-                } else {
-                    $("#comment_"+xhr.responseJSON.comment).slideUp(300, function() {
-                        $(this).remove();
-                    });
-                }
-            },
-            error: function(xhr, status, error) {
-                if (!xhr.responseJSON) {
-                    // TODO
-                    return;
-                }
+        var that = this;
+        var cmsCommentDeleteCheck = setInterval(function() {
+            if (cmsCommentLock == false) {
+                clearInterval(cmsCommentDeleteCheck);
+                cmsCommentLock = true;
+                cmsCommentDeleteSubmit(that);
             }
-        });
+        }, 10);
         return false;
     });
 }
