9f76dd4e13cf884c605bdd4c3b46e0000806e09a
Graham Campbell <graham@mineuk.com>
2013-11-02 11:25:32 +0000

    Typos again...

--- public/js/cms-comment-create.js
+++ public/js/cms-comment-create.js
@@ -35,19 +35,19 @@ function cmsCommentCreateSubmit(that) {
                 $("#nocomments").fadeOut(300, function() {
                     $(this).remove();
                     $(xhr.responseJSON.contents).prependTo('#comments').hide().fadeIn(300, function() {
-                        cmsTimeAgo("#timeago_comment_"+xhr.responseJSON.commnet_id);
-                        cmsCommentEdit("#editable_comment_"+xhr.responseJSON.commnet_id);
-                        cmsCommentDelete("#deletable_comment_"+xhr.responseJSON.commnet_id+"_1");
-                        cmsCommentDelete("#deletable_comment_"+xhr.responseJSON.commnet_id+"_2");
+                        cmsTimeAgo("#timeago_comment_"+xhr.responseJSON.comment_id);
+                        cmsCommentEdit("#editable_comment_"+xhr.responseJSON.comment_id);
+                        cmsCommentDelete("#deletable_comment_"+xhr.responseJSON.comment_id+"_1");
+                        cmsCommentDelete("#deletable_comment_"+xhr.responseJSON.comment_id+"_2");
                         cmsCommentLock = false;
                     });
                 });
             } else {
                 $(xhr.responseJSON.contents).prependTo('#comments').hide().slideDown(300, function() {
-                    cmsTimeAgo("#timeago_comment_"+xhr.responseJSON.commnet_id);
-                    cmsCommentEdit("#editable_comment_"+xhr.responseJSON.commnet_id);
-                    cmsCommentDelete("#deletable_comment_"+xhr.responseJSON.commnet_id+"_1");
-                    cmsCommentDelete("#deletable_comment_"+xhr.responseJSON.commnet_id+"_2");
+                    cmsTimeAgo("#timeago_comment_"+xhr.responseJSON.comment_id);
+                    cmsCommentEdit("#editable_comment_"+xhr.responseJSON.comment_id);
+                    cmsCommentDelete("#deletable_comment_"+xhr.responseJSON.comment_id+"_1");
+                    cmsCommentDelete("#deletable_comment_"+xhr.responseJSON.comment_id+"_2");
                     cmsCommentLock = false;
                 });
             }
