39425618e5972ec38dc5c471873b19a59ad008c6
Graham Campbell <graham@mineuk.com>
2013-11-02 01:07:56 +0000

    Revert some of the refactor

--- public/js/cms-comment-create.js
+++ public/js/cms-comment-create.js
@@ -2,58 +2,6 @@ function cmsCommentMessage(message, type) {
     $("#commentstatus").replaceWith("<label id=\"commentstatus\" class=\"has-"+type+"\"><div class=\"editable-error-block help-block\" style=\"display: block;\">"+message+"</div></label>");
 }
 
-function cmsCommentCreateSuccess(data, status, xhr) {
-    if (!xhr.responseJSON) {
-        cmsCommentMessage("There was an unknown error!", "error");
-        cmsCommentLock = false;
-        return;
-    }
-    if (xhr.responseJSON.success !== true) {
-        if (!xhr.responseJSON.msg) {
-            cmsCommentMessage("There was an unknown error!", "error");
-            cmsCommentLock = false;
-            return;
-        }
-        cmsCommentMessage(xhr.responseJSON.msg, "error");
-        cmsCommentLock = false;
-        return;
-    }
-    if (!xhr.responseJSON.msg || !xhr.responseJSON.comment) {
-        cmsCommentMessage("There was an unknown error!", "error");
-        cmsCommentLock = false;
-        return;
-    }
-    cmsCommentMessage(xhr.responseJSON.msg, "success");
-    if ($("#comments > div").length == 0) {
-        $("#nocomments").fadeOut(300, function() {
-            $(this).remove();
-            $(xhr.responseJSON.comment).prependTo('#comments').hide().fadeIn(300, function() {
-                cmsTimeAgo();
-                cmsCommentEdit();
-                cmsCommentDelete();
-                cmsCommentLock = false;
-            });
-        });
-    } else {
-        $(xhr.responseJSON.comment).prependTo('#comments').hide().slideDown(300, function() {
-            cmsTimeAgo();
-            cmsCommentEdit();
-            cmsCommentDelete();
-            cmsCommentLock = false;
-        });
-    }
-}
-
-function cmsCommentCreateError(xhr, status, error) {
-    if (!xhr.responseJSON || !xhr.responseJSON.msg) {
-        cmsCommentMessage("There was an unknown error!", "error");
-        cmsCommentLock = false;
-        return;
-    }
-    cmsCommentMessage(xhr.responseJSON.msg, "error");
-    cmsCommentLock = false;
-}
-
 function cmsCommentCreateSubmit(that) {
     cmsCommentMessage("Submitting comment...", "info");
     $(that).ajaxSubmit({ 
@@ -61,8 +9,57 @@ function cmsCommentCreateSubmit(that) {
         clearForm: true,
         resetForm: true,
         timeout: 5000,
-        success: cmsCommentCreateSuccess(data, status, xhr),
-        error: cmsCommentCreateError(xhr, status, error)
+        success: function(data, status, xhr) {
+            if (!xhr.responseJSON) {
+                cmsCommentMessage("There was an unknown error!", "error");
+                cmsCommentLock = false;
+                return;
+            }
+            if (xhr.responseJSON.success !== true) {
+                if (!xhr.responseJSON.msg) {
+                    cmsCommentMessage("There was an unknown error!", "error");
+                    cmsCommentLock = false;
+                    return;
+                }
+                cmsCommentMessage(xhr.responseJSON.msg, "error");
+                cmsCommentLock = false;
+                return;
+            }
+            if (!xhr.responseJSON.msg || !xhr.responseJSON.comment) {
+                cmsCommentMessage("There was an unknown error!", "error");
+                cmsCommentLock = false;
+                return;
+            }
+            cmsCommentMessage(xhr.responseJSON.msg, "success");
+            if ($("#comments > div").length == 0) {
+                $("#nocomments").fadeOut(300, function() {
+                    $(this).remove();
+                    $(xhr.responseJSON.comment).prependTo('#comments').hide().fadeIn(300, function() {
+                        cmsTimeAgo();
+                        cmsEditable();
+                        cmsCommentDelete();
+                        cmsCommentLock = false;
+                    });
+                });
+            } else {
+                $(xhr.responseJSON.comment).prependTo('#comments').hide().slideDown(300, function() {
+                    cmsTimeAgo();
+                    cmsEditable();
+                    cmsCommentDelete();
+                    cmsCommentLock = false;
+                });
+            }
+        },
+        error: function(xhr, status, error) {
+            if (!xhr.responseJSON || !xhr.responseJSON.msg) {
+                cmsCommentMessage("There was an unknown error!", "error");
+                cmsCommentLock = false;
+                return;
+            }
+            cmsCommentMessage(xhr.responseJSON.msg, "error");
+            cmsCommentLock = false;
+            return;
+        }
     });
 }
 
