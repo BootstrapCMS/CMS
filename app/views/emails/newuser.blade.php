633cec7a0ef4b0543bd8310a555b9a487186ef48
Graham Campbell <graham@mineuk.com>
2013-11-10 22:17:00 +0000

    Email tweaks

--- app/views/emails/newuser.blade.php
+++ app/views/emails/newuser.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.email')
 
 @section('content')
-<p>An admin from <a href="{{ $url }}">{{ Config::get('cms.name') }}</a> has created you an account.<p>
+<p>An admin from <a href="{{ $url }}">{{ Config::get('cms.name') }}</a> has created you an account.</p>
 <p>Here is your temporary password:</p>
 <p><blockquote>{{ $password }}</blockquote></p>
 <p>You should change it to something more memorable on the account page after you login.</p>
