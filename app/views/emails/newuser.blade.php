e24f1fa90e048942056bb6db21191dbeadfbef60
Graham Campbell <graham@mineuk.com>
2013-11-11 00:09:46 +0000

    Minor email tweaks

--- app/views/emails/newuser.blade.php
+++ app/views/emails/newuser.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <p>An admin from <a href="{{ $url }}">{{ Config::get('cms.name') }}</a> has created you an account.</p>
 <p>Here is your temporary password:</p>
-<p><blockquote>{{ $password }}</blockquote></p>
+<blockquote>{{ $password }}</blockquote>
 <p>You should change it to something more memorable on the account page after you login.</p>
 @stop
--- app/views/emails/password.blade.php
+++ app/views/emails/password.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <p>Here is your temporary password:</p>
-<p><blockquote>{{ $password }}</blockquote></p>
+<blockquote>{{ $password }}</blockquote>
 <p>You should change it to something more memorable on the account page after you login.</p>
 @stop
