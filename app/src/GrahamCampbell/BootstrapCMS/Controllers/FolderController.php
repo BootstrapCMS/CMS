0d66bd793a28124650054d7c02ca3079cce6fa90
Graham Campbell <graham@mineuk.com>
2013-11-13 21:50:15 +0000

    Updated the folder controller

--- app/src/GrahamCampbell/BootstrapCMS/Controllers/FolderController.php
+++ app/src/GrahamCampbell/BootstrapCMS/Controllers/FolderController.php
@@ -20,6 +20,13 @@
  * @link       https://github.com/GrahamCampbell/Bootstrap-CMS
  */
 
+use Binput;
+use Redirect;
+use FolderProvider;
+use Session;
+use Validator;
+
+use GrahamCampbell\CMSCore\Models\Folder;
 use GrahamCampbell\CMSCore\Controllers\BaseController;
 
 class FolderController extends BaseController {
@@ -44,5 +51,111 @@ class FolderController extends BaseController {
      */
     public function index() {
         return $this->viewMake('folders.index', array());
+    }
+
+    /**
+     * Show the form for creating a new folder.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        return $this->viewMake('folders.create');
+    }
+
+    /**
+     * Store a new folder.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store() {
+        $input = array(
+            'title'   => Binput::get('title'),
+            'summary' => Binput::get('summary'),
+            'user_id' => $this->getUserId()
+        );
+
+        $rules = Folder::$rules;
+
+        $val = Validator::make($input, $rules);
+        if ($val->fails()) {
+            return Redirect::route('folders.create')->withInput()->withErrors($val->errors());
+        }
+
+        $folder = FolderProvider::create($input);
+
+        // write flash message and redirect
+        Session::flash('success', 'Your folder has been created successfully.');
+        return Redirect::route('files.index', array('folders' => $folder->getId()));
+    }
+
+    /**
+     * Show the form for editing the specified folder.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {
+        $folder = Folder::find($id);
+        $this->checkFolder($folder);
+
+        return $this->viewMake('folders.edit', array('folder' => $folder));
+    }
+
+    /**
+     * Update an existing folder.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id) {
+        $input = array(
+            'title'   => Binput::get('title'),
+            'summary' => Binput::get('summary')
+        );
+
+        $rules = Folder::$rules;
+        unset($rules['user_id']);
+
+        $val = Validator::make($input, $rules);
+        if ($val->fails()) {
+            return Redirect::route('folders.edit')->withInput()->withErrors($val->errors());
+        }
+
+        $folder = FolderProvider::find($id);
+        $this->checkFolder($folder);
+
+        $folder->update($input);
+
+        // write flash message and redirect
+        Session::flash('success', 'Your folder has been updated successfully.');
+        return Redirect::route('files.index', array('folders' => $folder->getId()));
+    }
+
+    /**
+     * Delete an existing folder.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        $folder = FolderProvider::find($id);
+        $this->checkFolder($folder);
+
+        $folder->delete();
+
+        // write flash message and redirect
+        Session::flash('success', 'Your folder has been deleted successfully.');
+        return Redirect::route('folders.index');
+    }
+
+    /**
+     * Check the folder model.
+     *
+     * @return mixed
+     */
+    protected function checkFolder($folder) {
+        if (!$folder) {
+            return App::abort(404, 'Folder Not Found');
+        }
     }
 }
