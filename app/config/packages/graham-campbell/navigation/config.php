75a3082098fc17165f010b25eb711e53792460d6
Graham Campbell <graham@mineuk.com>
2013-12-07 20:07:23 +0000

    Navigation fixes

---
+++ app/config/packages/graham-campbell/navigation/config.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of Laravel Navigation by Graham Campbell.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    Laravel-Navigation
+ * @author     Graham Campbell
+ * @license    Apache License
+ * @copyright  Copyright 2013 Graham Campbell
+ * @link       https://github.com/GrahamCampbell/Laravel-Navigation
+ */
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation View
+    |--------------------------------------------------------------------------
+    |
+    | This option specifies the view to use.
+    |
+    | Default: 'navigation::bootstrap'
+    |
+    */
+
+    'view' => 'partials.navigation',
+
+);
--- app/views/partials/navigation.blade.php
+++ app/views/partials/navigation.blade.php
@@ -1,16 +1,16 @@
-<div class="navbar {{ (Config::get('theme.inverse') == true) ? 'navbar-inverse' : 'navbar-default'}} navbar-fixed-top">
+<div class="navbar {{ ($inverse == true) ? 'navbar-inverse' : 'navbar-default' }} navbar-fixed-top">
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="{{ URL::route('pages.show', array('pages' => 'home')) }}">{{ $site_name }}</a>
+        <a class="navbar-brand" href="{{ $main[0]['url'] }}">{{ $title }}</a>
     </div>
     <div class="collapse navbar-collapse">
         <div id="main-nav">
             <ul class="nav navbar-nav">
-                @foreach($nav_main as $item)
+                @foreach($main as $item)
                     <li{{ ($item['active'] ? ' class="active"' : '') }}>
                         <a href="{{ $item['url'] }}">
                             {{ ((!$item['icon'] == '') ? '<i class="fa fa-'.$item['icon'].' fa-inverse fa-fw"></i> ' : '') }}{{ $item['title'] }}
@@ -21,24 +21,19 @@
         </div>
         <div id="bar-nav">
             <ul class="nav navbar-nav navbar-right">
-                @if (!empty($nav_bar))
+                @if ($bar)
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            {{ Sentry::getUser()->email }} <b class="caret"></b>
+                            {{ $side }} <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu">
-                            @foreach($nav_bar as $item)
+                            @foreach($bar as $item)
                                 <li>
                                     <a href="{{ $item['url'] }}">
                                         {{ ((!$item['icon'] == '') ? '<i class="fa fa-'.$item['icon'].' fa-fw"></i> ' : '') }}{{ $item['title'] }}
                                     </a>
                                 </li>
                             @endforeach
-                            <li>
-                                <a href="#">
-                                    <i class="fa fa-envelope fa-fw"></i> Contact Support
-                                </a>
-                            </li>
                             <li class="divider"></li>
                             <li>
                                 <a href="{{ URL::route('account.logout') }}">
