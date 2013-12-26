636eb732fab256700bdbf310ebcdab205a2f5439
Graham Campbell <graham@mineuk.com>
2013-12-26 22:50:21 +0000

    Updated respond.js

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -8,7 +8,7 @@
 
 <!--[if lt IE 9]>
   <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-  <script src="//oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
 
 <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
