aefda271601b28964d13f05ce2b04fb428c14c90
Graham Campbell <graham@mineuk.com>
2013-12-31 17:51:22 +0000

    Added credentials config

---
+++ app/config/packages/graham-campbell/credentials/config.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of Laravel Credentials by Graham Campbell.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Public Registration
+    |--------------------------------------------------------------------------
+    |
+    | This defines if public registration is allowed.
+    |
+    | Default to true.
+    |
+    */
+
+    'regallowed' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Require Email Verification On Registration
+    |--------------------------------------------------------------------------
+    |
+    | This defines if public registration requires email activation.
+    |
+    | Default to true. 
+    |
+    */
+
+    'regemail' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login Page
+    |--------------------------------------------------------------------------
+    |
+    | This specifies the view that is used for the login page.
+    |
+    */
+
+    'login' => 'credentials::account.login',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Registration Page
+    |--------------------------------------------------------------------------
+    |
+    | This specifies the view that is used for the registration page.
+    |
+    */
+
+    'registration' => 'credentials::account.register'
+
+);
