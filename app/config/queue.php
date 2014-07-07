9fd694eac1c728c8f69f364efda17dc2c24253a0
Graham Campbell <graham@mineuk.com>
2014-07-07 20:08:40 +0100

    Minor fixes

--- app/config/queue.php
+++ app/config/queue.php
@@ -60,12 +60,12 @@ return array(
             'key'    => 'your-public-key',
             'secret' => 'your-secret-key',
             'queue'  => 'your-queue-url',
-            'region' => 'us-east-1'
+            'region' => 'eu-west-1'
         ),
 
         'iron' => array(
             'driver'  => 'iron',
-            'host'    => 'mq-aws-us-east-1.iron.io',
+            'host'    => 'mq-aws-eu-west-1.iron.io',
             'token'   => 'your-token',
             'project' => 'your-project-id',
             'queue'   => 'your-queue-name',
