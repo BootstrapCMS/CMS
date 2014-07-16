d44832cdb17e946bbd327d10b2a270bc901b348e
Graham Campbell <graham@mineuk.com>
2014-07-16 14:56:16 +0100

    Remove unused filters

--- app/filters.php
+++ app/filters.php
@@ -38,48 +38,6 @@ App::after(function ($request, $response) {
 
 /*
 |--------------------------------------------------------------------------
-| Authentication Filters
-|--------------------------------------------------------------------------
-|
-| The following filters are used to verify that the user of the current
-| session is logged into this application. The "basic" filter easily
-| integrates HTTP Basic authentication for quick, simple checking.
-|
-*/
-
-Route::filter('auth', function () {
-    if (Auth::guest()) {
-        if (Request::ajax()) {
-            throw new UnauthorizedHttpException('Action Requires Login');
-        }
-        return Redirect::guest('login');
-    }
-});
-
-
-Route::filter('auth.basic', function () {
-    return Auth::basic();
-});
-
-/*
-|--------------------------------------------------------------------------
-| Guest Filter
-|--------------------------------------------------------------------------
-|
-| The "guest" filter is the counterpart of the authentication filters as
-| it simply checks that the current user is not logged in. A redirect
-| response will be issued if they are, which you may freely change.
-|
-*/
-
-Route::filter('guest', function () {
-    if (!Request::ajax() && Auth::check()) {
-        return Redirect::intended(URL::to(Config::get('graham-campbell/core::home', 'pages/home')));
-    }
-});
-
-/*
-|--------------------------------------------------------------------------
 | CSRF Protection Filter
 |--------------------------------------------------------------------------
 |
