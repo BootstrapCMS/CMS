55d68dcdfc8494755e809c5d185dc50c6fa5f867
Graham Campbell <graham@mineuk.com>
2014-07-19 18:04:12 +0100

    Work on page history

---
+++ src/Presenters/RevisionDisplayers/Page/CssDisplayer.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of Bootstrap CMS by Graham Campbell.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
+namespace GrahamCampbell\BootstrapCMS\Presenters\RevisionDisplayers\Page;
+
+/**
+ * This is the css displayer class.
+ *
+ * @package    Bootstrap-CMS
+ * @author     Graham Campbell
+ * @copyright  Copyright (C) 2013-2014  Graham Campbell
+ * @license    https://github.com/GrahamCampbell/Bootstrap-CMS/blob/master/LICENSE.md
+ * @link       https://github.com/GrahamCampbell/Bootstrap-CMS
+ */
+class CssDisplayer extends AbstractDisplayer
+{
+    /**
+     * Get the change description from the context of
+     * the change being made by the current user.
+     *
+     * @return string
+     */
+    protected function current()
+    {
+        return 'You updated the css section of' . $this->name();
+    }
+
+    /**
+     * Get the change description from the context of
+     * the change not being made by the current user.
+     *
+     * @return string
+     */
+    protected function external()
+    {
+        return 'This user updated the css section of' . $this->name();
+    }
+}
