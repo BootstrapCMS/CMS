f68ae5000a2d5ddee64d6dfcd365ddfc7e94919b
Graham Campbell <graham@mineuk.com>
2014-11-02 12:21:40 +0000

    Asset updates

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,13 +3,13 @@
 <meta name="author" content="{{ Config::get('platform.author') }}">
 
 {!! HTML::style('//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css') !!}
-{!! HTML::style('//cdnjs.cloudflare.com/ajax/libs/animate.css/3.1.1/animate.min.css') !!}
+{!! HTML::style('//cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.0/animate.min.css') !!}
 {!! Asset::styles('main') !!}
 @section('css')
 @show
 
 <!--[if lt IE 9]>
-  {!! HTML::script('//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.min.js') !!}
+  {!! HTML::script('//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js') !!}
   {!! HTML::script('//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js') !!}
 <![endif]-->
 
