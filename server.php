5c15303c9d13e8bc4324d8c8ffe6d7c3004d6ae8
Graham Campbell <graham@mineuk.com>
2014-11-05 22:24:25 +0000

    Fixed php artisan serve

--- server.php
+++ server.php
@@ -1,12 +1,26 @@
 <?php
 
+/**
+ * This file is part of Bootstrap CMS by Graham Campbell.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = urldecode($uri);
 
-$paths = require __DIR__.'/bootstrap/paths.php';
+$public = __DIR__ . '/public';
 
-$requested = $paths['public'].$uri;
+$requested = $public . $uri;
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
@@ -15,4 +29,4 @@ if ($uri !== '/' and file_exists($requested)) {
     return false;
 }
 
-require_once $paths['public'].'/index.php';
+require_once $public . '/index.php';
