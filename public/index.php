a6cfb7d1268457c06504bd0d740d70b451ab1e55
Graham Campbell <graham@mineuk.com>
2014-11-07 20:17:04 +0000

    Updated index.php

--- public/index.php
+++ public/index.php
@@ -54,6 +54,10 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-$response = $app->make('Illuminate\Contracts\Http\Kernel')->handle(Illuminate\Http\Request::capture());
+$kernel = $app->make('Illuminate\Contracts\Http\Kernel');
+
+$response = $kernel->handle($request = Illuminate\Http\Request::capture());
 
 $response->send();
+
+$kernel->terminate($request, $response);
