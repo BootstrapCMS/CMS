70979608ab319fe356ef7716401530f435a5d412
Graham Campbell <graham@mineuk.com>
2014-11-19 22:33:11 +0000

    Removed a duplicate tag

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -2,7 +2,6 @@
 <html lang="en-GB">
 <head>
 <meta charset="utf-8">
-<meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <title>Site Maintenance</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
