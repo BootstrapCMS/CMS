9e9aae17f9eaaca923380be6ebe447145dc48e72
Graham Campbell <graham@mineuk.com>
2014-12-24 01:35:22 +0000

    CS fix

--- config/packages/cartalyst/sentry/config.php
+++ config/packages/cartalyst/sentry/config.php
@@ -1,5 +1,6 @@
 <?php
-/**
+
+/*
  * Part of the Sentry package.
  *
  * NOTICE OF LICENSE
@@ -8,13 +9,7 @@
  *
  * This source file is subject to the 3-clause BSD License that is
  * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
- *
- * @package    Sentry
- * @author     Cartalyst LLC
- * @license    BSD License (3-clause)
- * @copyright  (c) 2011 - 2013, Cartalyst LLC
- * @link       http://cartalyst.com
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause.
  */
 
 return [
