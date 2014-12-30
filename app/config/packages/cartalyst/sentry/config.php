c1f8170aba8e5debac9a726ee8d99305a832e757
Graham Campbell <graham@mineuk.com>
2014-12-30 18:40:54 +0000

    CS fixes

--- app/config/packages/cartalyst/sentry/config.php
+++ app/config/packages/cartalyst/sentry/config.php
@@ -10,12 +10,9 @@
  * bundled with this package in the LICENSE file.  It is also available at
  * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
  *
- * @package    Sentry
- * @version    2.1.0
  * @author     Cartalyst LLC
  * @license    BSD License (3-clause)
  * @copyright  (c) 2011 - 2013, Cartalyst LLC
- * @link       http://cartalyst.com
  */
 return [
 
