4c18d7347df5c7b9fece4ae47df0d0e4c8031f84
Graham Campbell <graham@mineuk.com>
2015-01-04 00:40:36 +0000

    Updated file headers

--- server.php
+++ server.php
@@ -1,17 +1,12 @@
 <?php
 
 /*
- * This file is part of Bootstrap CMS by Graham Campbell.
+ * This file is part of Bootstrap CMS.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * (c) Graham Campbell <graham@mineuk.com>
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
