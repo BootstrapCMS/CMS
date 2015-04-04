fa1407b59de940282a2f419357f6823d9a20696c
Graham Campbell <graham@mineuk.com>
2015-04-04 17:03:00 +0100

    Updated autoload.php

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -36,8 +36,8 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$compiledPath = __DIR__.'/../vendor/compiled.php';
-
-if (file_exists($compiledPath)) {
-    require $compiledPath;
+if (file_exists($compiledPath = __DIR__.'/../vendor/compiled.php')) {
+	require $compiledPath;
+} elseif (file_exists($compiledPath = __DIR__.'/../storage/framework/compiled.php')) {
+	require $compiledPath;
 }
