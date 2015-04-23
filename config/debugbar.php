c1fdbded5000e95abbcafcf5954dd74871656a71
Graham Campbell <graham@mineuk.com>
2015-04-23 09:08:10 +0100

    Fixed debugbar config

--- config/debugbar.php
+++ config/debugbar.php
@@ -117,9 +117,9 @@ return [
         'db' => [
             'with_params' => true,  // Render SQL with the parameters substituted
             'timeline'    => false, // Add the queries to the timeline
-            'backtrace'   => false, // EXPERIMENTAL: Use a backtrace to find the origin of the query in your files.
-            'explain'     => [      // EXPERIMENTAL: Show EXPLAIN output on queries
-                'enabled' => false,
+            'backtrace'   => false, // EXPERIMENTAL: Use a backtrace to find the origin of the query in your files
+            'explain'     => [
+                'enabled' => false,      // EXPERIMENTAL: Show EXPLAIN output on queries
                 'types'   => ['SELECT'], // array('SELECT', 'INSERT', 'UPDATE', 'DELETE'); for MySQL 5.6.3+
             ],
             'hints' => true, // Show hints for common mistakes
@@ -128,10 +128,10 @@ return [
             'full_log' => false,
         ],
         'views' => [
-            'data' => false, // Note: Can slow down the application, because the data can be quite large..
+            'data' => false, // Note: Can slow down the application, because the data can be quite large...
         ],
         'route' => [
-            'label' => true, // show complete route on bar
+            'label' => true, // Show complete route on bar
         ],
         'logs' => [
             'file' => null,
