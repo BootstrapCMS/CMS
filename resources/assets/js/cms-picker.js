153729da8191a3229457863005d03128639cbbcc
Graham Campbell <graham@alt-three.com>
2015-08-10 20:16:47 +0100

    Cleanup cms picker js

--- resources/assets/js/cms-picker.js
+++ resources/assets/js/cms-picker.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-  if (typeof js_datetime_format === 'undefined') {
-    js_datetime_format = 'D/M/YYYY HH:mm';
-  }
-  $('#datetimepicker1').datetimepicker({
-    format: js_datetime_format
-  });
+    $('#datetimepicker1').datetimepicker({
+        format: js_datetime_format
+    });
 });
