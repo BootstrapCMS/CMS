20c835af6ddcb654b1b93f93cfa00f7bfbbcb37c
Graham Campbell <graham@alt-three.com>
2015-08-10 20:22:32 +0100

    Fixed the encryption cipher used

--- config/app.php
+++ config/app.php
@@ -101,7 +101,7 @@ return [
 
     'key' => env('APP_KEY', 'SomeRandomString'),
 
-    'cipher' => MCRYPT_RIJNDAEL_128,
+    'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
