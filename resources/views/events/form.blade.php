069235a8ec5f8f321806f7f9ebe8942b0fea9cbb
Drethic <drethic@msn.com>
2015-09-05 21:50:42 +0000

    Applied xcaptain's change to Events.

--- resources/views/events/form.blade.php
+++ resources/views/events/form.blade.php
@@ -1,6 +1,7 @@
 <form class="form-horizontal" action="{{ $form['url'] }}" method="{{ $form['method'] }}">
 
     {{ csrf_field() }}
+    <input type="hidden" name="_method" value="{{ $form['_method'] }}">
 
     <div class="form-group{!! ($errors->has('title')) ? ' has-error' : '' !!}">
         <label class="col-md-2 col-sm-3 col-xs-10 control-label" for="title">Event Title</label>
