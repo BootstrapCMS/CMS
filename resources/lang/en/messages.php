950c7c412d214aa3d99639f6a9974c71099b13f8
Abu Baker Siddique <abskomol@gmail.com>
2016-06-28 15:46:13 +0600

    Fix StyleCI

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -8,39 +8,39 @@ return [
     |
     */
 
-    'comment' => [
-        'view_error' => 'The post you were viewing has been deleted.',
-        'empty' => 'Your comment was empty.',
-        'store_success' => 'Comment created successfully.',
-        'no_version_data' => 'No version data was supplied.',
-        'version_mismatch' => 'The comment was modified by someone else.',
-        'update_success' => 'Comment updated successfully.',
-        'delete_success' => 'Comment deleted successfully.',
-        'not_found' => 'Comment Not Found.'
+    'comment'   => [
+        'view_error'        => 'The post you were viewing has been deleted.',
+        'empty'             => 'Your comment was empty.',
+        'store_success'     => 'Comment created successfully.',
+        'no_version_data'   => 'No version data was supplied.',
+        'version_mismatch'  => 'The comment was modified by someone else.',
+        'update_success'    => 'Comment updated successfully.',
+        'delete_success'    => 'Comment deleted successfully.',
+        'not_found'         => 'Comment Not Found.'
     ],
 
-    'event' => [
-        'store_success' => 'Your event has been created successfully.',
-        'update_success' => 'Your event has been updated successfully.',
-        'delete_success' => 'Your event has been deleted successfully.',
-        'not_found' => 'Event Not Found'
+    'event'     => [
+        'store_success'     => 'Your event has been created successfully.',
+        'update_success'    => 'Your event has been updated successfully.',
+        'delete_success'    => 'Your event has been deleted successfully.',
+        'not_found'         => 'Event Not Found'
     ],
 
-    'page' => [
-        'store_success' => 'Your page has been created successfully.',
-        'update_success' => 'Your page has been updated successfully.',
-        'delete_success' => 'Your page has been deleted successfully.',
-        'delete_error' => 'You cannot delete this page.',
-        'homepage_missing' => 'The homepage is missing.',
-        'not_found' => 'Page Not Found',
-        'homepage_slug' => 'You cannot change the homepage slug.',
-        'show_nav' => 'The homepage must remain on the navigation bar.'
+    'page'      => [
+        'store_success'     => 'Your page has been created successfully.',
+        'update_success'    => 'Your page has been updated successfully.',
+        'delete_success'    => 'Your page has been deleted successfully.',
+        'delete_error'      => 'You cannot delete this page.',
+        'homepage_missing'  => 'The homepage is missing.',
+        'not_found'         => 'Page Not Found',
+        'homepage_slug'     => 'You cannot change the homepage slug.',
+        'show_nav'          => 'The homepage must remain on the navigation bar.'
     ],
 
-    'post' => [
-        'store_success' => 'Your post has been created successfully.',
-        'update_success' => 'Your post has been updated successfully.',
-        'delete_success' => 'Your post has been deleted successfully.',
-        'not_found' => 'Post Not Found'
+    'post'      => [
+        'store_success'     => 'Your post has been created successfully.',
+        'update_success'    => 'Your post has been updated successfully.',
+        'delete_success'    => 'Your post has been deleted successfully.',
+        'not_found'         => 'Post Not Found'
     ]
 ];
